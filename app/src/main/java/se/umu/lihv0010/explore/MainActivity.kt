2f483e3a80bfd1e781da54d20cf617ac24bf1348
Linus Hvenfelt <linushvenfelt@gmail.com>
2022-07-02 20:05:49 +0200

    first commit before creating more functionality

--- app/src/main/java/se/umu/lihv0010/explore/MainActivity.kt
+++ app/src/main/java/se/umu/lihv0010/explore/MainActivity.kt
@@ -3,11 +3,14 @@ package se.umu.lihv0010.explore
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
-import android.graphics.Color
 import android.location.LocationManager
 import android.os.Bundle
 import android.os.Looper
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.util.Log
 import android.view.MotionEvent
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.preference.PreferenceManager
@@ -16,22 +19,25 @@ import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
-import org.osmdroid.api.IMapController
+import org.osmdroid.bonuspack.routing.OSRMRoadManager
+import org.osmdroid.bonuspack.routing.RoadManager
 import org.osmdroid.config.Configuration.getInstance
 import org.osmdroid.events.MapEventsReceiver
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
-import org.osmdroid.util.BoundingBox
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.MapEventsOverlay
 import org.osmdroid.views.overlay.Marker
-import org.osmdroid.views.overlay.Polygon
 import org.osmdroid.views.overlay.Polyline
-
-// TODO: Following tutorial https://www.geeksforgeeks.org/how-to-get-current-location-in-android/
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+import se.umu.lihv0010.explore.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var map : MapView
+    private lateinit var binding: ActivityMainBinding
+    private val TAG = "DebugExplore"
 
     private val locationRequest = com.google.android.gms.location.LocationRequest.create().apply {
         interval = 200
@@ -39,173 +45,58 @@ class MainActivity : AppCompatActivity() {
         maxWaitTime = 200
         priority = PRIORITY_HIGH_ACCURACY
     }
-
     lateinit var locationManager: LocationManager
     private var latestLocation: GeoPoint = GeoPoint(57.86973548791104, 11.974444448918751)
-    private lateinit var mapController: IMapController
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var locationCallback: LocationCallback
+    private lateinit var myLocationOverlay: MyLocationNewOverlay
 
-    private var userCentered: Boolean = true
-
-    private var xCoordinate: Float = 0F
-    private var yCoordinate: Float = 0F
-
+    //private var xCoordinate: Float = 0F
+    //private var yCoordinate: Float = 0F
 
     // TODO: Center map button to set userCentered = true
-
     // TODO: Implement https://github.com/MKergall/osmbonuspack
-
     // TODO: Placing markers functionability
     // TODO: Fog of war
     // TODO: Get popular landmarks from OSM
 
-    private lateinit var centerDot: Marker
-    private var earthCover = Polygon()
-
     override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
-        xCoordinate = ev.x
-        yCoordinate = ev.y
-        userCentered = false
+        //xCoordinate = ev.x
+        //yCoordinate = ev.y
+        myLocationOverlay.disableFollowLocation() // Stops following center marker
+        binding.centerButton.visibility = View.VISIBLE // Shows a center button
         return super.dispatchTouchEvent(ev)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        // https://github.com/MKergall/osmbonuspack/wiki/Tutorial_0
+        // https://stackoverflow.com/questions/21213224/roadmanager-for-osmdroid-error
+        val policy = ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
+        getInstance().userAgentValue = "ExploreApp/1.0"
         getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        map = findViewById(R.id.mapview)
+        setupMap()
+        setupTouchListeners()
+        setupUI()
+    }
+
+    private fun setupMap() {
+        Log.d(TAG, "Setting up map!")
+        map = binding.mapview
         map.setMultiTouchControls(true)
+        map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
+        map.controller.setZoom(18.0) // Should be 18
 
-
-        initialMapControls()
         initLocationListener()
-        createLocationMarker()
-        loadFogOfWar()
-        setupListener()
-    }
-
-    private fun setupListener() {
-        val mReceive: MapEventsReceiver = object : MapEventsReceiver {
-            override fun singleTapConfirmedHelper(p: GeoPoint): Boolean {
-                println("SingleTapConfirmedHelper")
-
-                val circle = Polygon(map)
-                circle.points = Polygon.pointsAsCircle(p, 5000.0)
-                circle.fillColor = (Color.TRANSPARENT)
-                circle.setStrokeColor(Color.RED)
-                map.overlays.add(circle)
-                map.invalidate()
-
-
-                return false
-            }
-
-            override fun longPressHelper(p: GeoPoint): Boolean {
-                println("longPressHelper")
-                //placeMarker(p) // TODO
-                return false
-            }
-        }
-        val overlayEvents = MapEventsOverlay(mReceive)
-        map.overlays.add(overlayEvents)
-    }
-
-    private fun loadFogOfWar() {
-        val geoPoints: MutableList<GeoPoint> = arrayListOf(
-            GeoPoint(53.225768435790194, -0.087890625),
-            GeoPoint(53.225768435790194,36.9140625),
-            GeoPoint(71.49703690095419,36.9140625),
-            GeoPoint(71.49703690095419,-0.087890625),
-            GeoPoint(53.225768435790194, -0.087890625)
-        )
-        earthCover.fillColor = (-0xFA0000) // Black color fog, TODO: Animate?
-        earthCover.points = geoPoints
-
-        /*
-        val holes: MutableList<List<GeoPoint>> = ArrayList()
-        holes.add(uncoveredFog)
-        earthCover.holes = holes
-        map.overlayManager.add(earthCover)
-        map.invalidate()
-         */
-    }
-
-    private var locationsVisited: MutableList<GeoPoint> = mutableListOf()
-
-    private fun removeFog(location: GeoPoint) {
-        println("Removing fog from new area")
-
-        /*
-        val oldBox = BoundingBox.fromGeoPoints(uncoveredFog)
-        for (newPoint in polygon.points) {
-            if (oldBox.contains(newPoint)) {
-
-                val newBox = BoundingBox.fromGeoPoints(polygon.actualPoints)
-
-
-                for ((index, oldPoint) in uncoveredFog.withIndex()) {
-                    if (newBox.contains(oldPoint)) {
-                        println("New box contains old point in it")
-
-                        val newList = uncoveredFog.toMutableList()
-                        newList.remove(oldPoint)
-                        uncoveredFog = newList as ArrayList<GeoPoint>
-                    }
-                }
-
-
-            } else {
-                uncoveredFog.add(newPoint)
-            }
-        }*/
-
-        /* POLYLINE
-        locationsVisited.add(location)
-        var line = Polyline()
-        line.setPoints(locationsVisited)
-        map.overlays.add(line)
-        */
-
-        /*
-        val polygon = Polygon() //see note below
-        var geoPoints = Polygon.pointsAsRect(location, 200.0, 200.0) as MutableList<GeoPoint>
-        polygon.fillColor = Color.argb(0, 255, 0, 0)
-        polygon.strokeColor = Color.argb(0, 0, 0, 0)
-        geoPoints.add(geoPoints[0]) //forces the loop to close
-        polygon.points = geoPoints
-
-
-        val holes: MutableList<List<GeoPoint>> = ArrayList()
-        holes.add(geoPoints)
-        earthCover.holes = holes
-         */
-    }
-
-    private fun placeMarker(p: GeoPoint) {
-        var newMarker = Marker(map)
-        newMarker.position = p
-        //newMarker.icon =
-        map.overlays.add(newMarker)
-        map.invalidate()
-    }
-
-    private fun createLocationMarker() {
-        centerDot = Marker(map)
-        centerDot.position = latestLocation
-        centerDot.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        map.overlays.add(centerDot)
-        map.invalidate()
-    }
-
-    private fun initialMapControls() {
-        map.setTileSource(TileSourceFactory.MAPNIK)
-        mapController = map.controller
-        mapController.setZoom(18.0) // Should be 18
     }
 
     private fun initLocationListener() {
+        // TODO: Duplicate looking for location atm, both this and mylocationoverlay
         @SuppressLint("MissingPermission")
         if (isLocationPermissionGranted()) {
             locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
@@ -218,13 +109,7 @@ class MainActivity : AppCompatActivity() {
                         if (result != null) {
                             val newLocation = GeoPoint(result.latitude, result.longitude)
                             if (newLocation != latestLocation) {
-                                println("Setting new location")
-                                setLocation(newLocation)
-
-                                centerDot.position = latestLocation
-
-                                removeFog(newLocation)
-
+                                latestLocation = newLocation
                             }
                         }
 
@@ -232,14 +117,73 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+        } else {
+            Log.d(TAG, "Location services not granted")
+        }
+
+        if (isLocationPermissionGranted()) {
+            myLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), map)
+            myLocationOverlay.enableMyLocation()
+            myLocationOverlay.enableFollowLocation()
+            map.overlays.add(myLocationOverlay)
+            map.invalidate()
         }
     }
 
-    private fun setLocation(newLocation: GeoPoint) {
-        latestLocation = newLocation
-        if (userCentered) {
-            mapController.animateTo(newLocation)
+    private fun setupTouchListeners() {
+        val mReceive: MapEventsReceiver = object : MapEventsReceiver {
+            override fun singleTapConfirmedHelper(p: GeoPoint): Boolean {
+                Log.d(TAG, "Single tap")
+                return false
+            }
+
+            override fun longPressHelper(p: GeoPoint): Boolean {
+                Log.d(TAG, "Long press")
+                //placeMarker(p) // TODO
+                return false
+            }
         }
+        val overlayEvents = MapEventsOverlay(mReceive)
+        map.overlays.add(overlayEvents)
+    }
+
+    private fun setupUI() {
+        binding.testButton.setOnClickListener {
+            spawnGoal()
+        }
+        binding.centerButton.setOnClickListener {
+            myLocationOverlay.enableFollowLocation()
+            binding.centerButton.visibility = View.GONE
+        }
+    }
+
+    private fun placeMarker(p: GeoPoint) {
+        var newMarker = Marker(map)
+        newMarker.position = p
+        map.overlays.add(newMarker)
+        map.invalidate()
+    }
+
+    private fun spawnGoal() {
+        println("Spawning goal!")
+        val random = Random(System.currentTimeMillis())
+        var oldLocation: GeoPoint = latestLocation
+        val newGoal = Marker(map)
+        newGoal.position = oldLocation.destinationPoint(500.0, random.nextDouble(0.0, 360.0))
+        map.overlays.add(newGoal)
+        map.invalidate()
+
+        pathToGoal(newGoal.position)
+    }
+
+    private fun pathToGoal(newGoal: GeoPoint) {
+        val roadManager: RoadManager = OSRMRoadManager(this, "ExploreApp/1.0")
+        (roadManager as OSRMRoadManager).setMean(OSRMRoadManager.MEAN_BY_FOOT)
+        val waypoints: ArrayList<GeoPoint> = arrayListOf(latestLocation, newGoal)
+        val path = roadManager.getRoad(waypoints)
+        val pathOverlay: Polyline = RoadManager.buildRoadOverlay(path)
+        map.overlays.add(pathOverlay)
+        map.invalidate()
     }
 
     private fun isLocationPermissionGranted(): Boolean {
@@ -265,14 +209,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        map.onResume()
-    }
-    override fun onPause() {
-        super.onPause()
-        map.onPause()
-    }
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         val permissionsToRequest = ArrayList<String>()
@@ -286,5 +222,14 @@ class MainActivity : AppCompatActivity() {
                 this,
                 permissionsToRequest.toTypedArray(), 1)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        map.onResume()
+    }
+    override fun onPause() {
+        super.onPause()
+        map.onPause()
     }
 }
---
+++ app/src/main/java/se/umu/lihv0010/explore/ScoreSystem.kt
@@ -0,0 +1,10 @@
+package se.umu.lihv0010.explore
+
+import androidx.fragment.app.Fragment
+
+class ScoreSystem : Fragment() {
+
+    val score = 0
+
+
+}
