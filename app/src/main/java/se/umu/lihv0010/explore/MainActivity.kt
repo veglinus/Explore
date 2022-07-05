447d58e3a41feae887e5025d5a26a3a5b367df2f
Linus Hvenfelt <linushvenfelt@gmail.com>
2022-07-05 14:14:39 +0200

    goal and reaching goal

--- app/src/main/java/se/umu/lihv0010/explore/MainActivity.kt
+++ app/src/main/java/se/umu/lihv0010/explore/MainActivity.kt
@@ -11,6 +11,7 @@ import android.os.StrictMode.ThreadPolicy
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.preference.PreferenceManager
@@ -34,10 +35,11 @@ import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 import se.umu.lihv0010.explore.databinding.ActivityMainBinding
 import kotlin.random.Random
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var map : MapView
     private lateinit var binding: ActivityMainBinding
-    private val TAG = "DebugExplore"
+    private val tag = "DebugExplore"
 
     private val locationRequest = com.google.android.gms.location.LocationRequest.create().apply {
         interval = 200
@@ -45,20 +47,25 @@ class MainActivity : AppCompatActivity() {
         maxWaitTime = 200
         priority = PRIORITY_HIGH_ACCURACY
     }
+
     lateinit var locationManager: LocationManager
+    private var homeLocation = GeoPoint(57.86973548791104, 11.974444448918751)
     private var latestLocation: GeoPoint = GeoPoint(57.86973548791104, 11.974444448918751)
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var locationCallback: LocationCallback
     private lateinit var myLocationOverlay: MyLocationNewOverlay
 
-    //private var xCoordinate: Float = 0F
-    //private var yCoordinate: Float = 0F
+    private val random = Random(System.currentTimeMillis())
+    private var latestGoalDirection = random.nextDouble(0.0, 360.0)
 
-    // TODO: Center map button to set userCentered = true
-    // TODO: Implement https://github.com/MKergall/osmbonuspack
-    // TODO: Placing markers functionability
-    // TODO: Fog of war
-    // TODO: Get popular landmarks from OSM
+    private var goals: MutableList<GeoPoint> = mutableListOf()
+    private var visitedGoals: MutableList<GeoPoint> = mutableListOf() // Delete each day
+    private var points: Int = 0
+
+    // TODO: Zoom to new goal, show the description and points on it
+    // TODO: Cancel button for goal (maybe allow user to finish x meters away for less points)
+
+    // TODO: Achievements
 
     override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
         //xCoordinate = ev.x
@@ -71,10 +78,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        // https://github.com/MKergall/osmbonuspack/wiki/Tutorial_0
-        // https://stackoverflow.com/questions/21213224/roadmanager-for-osmdroid-error
-        val policy = ThreadPolicy.Builder().permitAll().build()
-        StrictMode.setThreadPolicy(policy)
+
+        val policy = ThreadPolicy.Builder().permitAll().build() // https://github.com/MKergall/osmbonuspack/wiki/Tutorial_0
+        StrictMode.setThreadPolicy(policy) // https://stackoverflow.com/questions/21213224/roadmanager-for-osmdroid-error
 
         getInstance().userAgentValue = "ExploreApp/1.0"
         getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
@@ -86,7 +92,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupMap() {
-        Log.d(TAG, "Setting up map!")
+        Log.d(tag, "Setting up map!")
         map = binding.mapview
         map.setMultiTouchControls(true)
         map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
@@ -96,7 +102,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initLocationListener() {
-        // TODO: Duplicate looking for location atm, both this and mylocationoverlay
+        // TODO: Duplicate looking for location atm, both this and my location overlay
         @SuppressLint("MissingPermission")
         if (isLocationPermissionGranted()) {
             locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
@@ -110,6 +116,8 @@ class MainActivity : AppCompatActivity() {
                             val newLocation = GeoPoint(result.latitude, result.longitude)
                             if (newLocation != latestLocation) {
                                 latestLocation = newLocation
+
+                                checkIfGoalReached(newLocation);
                             }
                         }
 
@@ -118,7 +126,7 @@ class MainActivity : AppCompatActivity() {
             }
             fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
         } else {
-            Log.d(TAG, "Location services not granted")
+            Log.d(tag, "Location services not granted")
         }
 
         if (isLocationPermissionGranted()) {
@@ -130,15 +138,36 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun checkIfGoalReached(location: GeoPoint) {
+        for (goal in goals) {
+            if (location.distanceToAsDouble(goal) < 50.0) {
+
+                goals.remove(goal)
+                visitedGoals.add(goal)
+
+                // TODO: Need to find exact goal and remove it, so we can have multiple
+                map.overlays.removeLast()
+                map.overlays.removeLast()
+
+                Toast.makeText(this, "Goal reached! Points have been added.", Toast.LENGTH_LONG).show()
+                addPoints()
+            }
+        }
+    }
+
+    private fun addPoints() {
+        points += 10
+    }
+
     private fun setupTouchListeners() {
         val mReceive: MapEventsReceiver = object : MapEventsReceiver {
             override fun singleTapConfirmedHelper(p: GeoPoint): Boolean {
-                Log.d(TAG, "Single tap")
+                Log.d(tag, "Single tap")
                 return false
             }
 
             override fun longPressHelper(p: GeoPoint): Boolean {
-                Log.d(TAG, "Long press")
+                Log.d(tag, "Long press")
                 //placeMarker(p) // TODO
                 return false
             }
@@ -149,7 +178,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupUI() {
         binding.testButton.setOnClickListener {
-            spawnGoal()
+            spawnGoal(500.0)
         }
         binding.centerButton.setOnClickListener {
             myLocationOverlay.enableFollowLocation()
@@ -158,32 +187,77 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun placeMarker(p: GeoPoint) {
-        var newMarker = Marker(map)
+        val newMarker = Marker(map)
         newMarker.position = p
         map.overlays.add(newMarker)
         map.invalidate()
     }
 
-    private fun spawnGoal() {
-        println("Spawning goal!")
-        val random = Random(System.currentTimeMillis())
-        var oldLocation: GeoPoint = latestLocation
+    private fun goHome() {
         val newGoal = Marker(map)
-        newGoal.position = oldLocation.destinationPoint(500.0, random.nextDouble(0.0, 360.0))
+        newGoal.position = getClosestRoadAndPath(homeLocation) // TODO: Find the 500m line on this path and pick that instead of full path
+        goals.add(newGoal.position)
         map.overlays.add(newGoal)
         map.invalidate()
-
-        pathToGoal(newGoal.position)
     }
 
-    private fun pathToGoal(newGoal: GeoPoint) {
+    private fun randomDirection(): Double {
+        val rnd2 = random.nextBoolean()
+        var newDirection = latestGoalDirection
+
+        val newTurn = random.nextInt(45, 90)
+
+        if (rnd2) {
+            newDirection += newTurn
+        } else {
+            newDirection -= newTurn
+        }
+
+        if (newDirection > 360.0) {
+            newDirection -= 360
+        } else if (newDirection < 0) {
+            newDirection += 360
+        }
+
+        Log.d(tag, "Random direction generated! $newDirection")
+        latestGoalDirection = newDirection // For next generation to be 90 degrees off last one
+        return newDirection
+    }
+
+    private fun spawnGoal(distance: Double) {
+        println("Spawning goal!")
+        val newGoal = Marker(map)
+        val randomPoint = latestLocation.destinationPoint(distance, randomDirection())
+
+        // TODO: Check if new marker is within 300m, if it is, generate a new one til we get there
+        // TODO: Also check so that the latest point isnt the same as one that has been done before, within x meters
+
+        newGoal.position = getClosestRoadAndPath(randomPoint) // TODO: Find the 500m line on this path and pick that instead of full path
+
+        /*
+        while (latestLocation.distanceToAsDouble(newGoal.position) < distance * 0.9) {
+
+        }*/
+        
+        goals.add(newGoal.position)
+
+        map.overlays.add(newGoal)
+        map.invalidate()
+    }
+
+    private fun getClosestRoadAndPath(newGoal: GeoPoint): GeoPoint {
+        // Creates path from start to finish
         val roadManager: RoadManager = OSRMRoadManager(this, "ExploreApp/1.0")
         (roadManager as OSRMRoadManager).setMean(OSRMRoadManager.MEAN_BY_FOOT)
         val waypoints: ArrayList<GeoPoint> = arrayListOf(latestLocation, newGoal)
         val path = roadManager.getRoad(waypoints)
+        path.mLength = 500.0
         val pathOverlay: Polyline = RoadManager.buildRoadOverlay(path)
+
+
+
         map.overlays.add(pathOverlay)
-        map.invalidate()
+        return pathOverlay.actualPoints.last() // Returns our new point, on a reachable road
     }
 
     private fun isLocationPermissionGranted(): Boolean {
--- app/src/main/java/se/umu/lihv0010/explore/ScoreSystem.kt
+++ app/src/main/java/se/umu/lihv0010/explore/ScoreSystem.kt
@@ -1,8 +1,6 @@
 package se.umu.lihv0010.explore
 
-import androidx.fragment.app.Fragment
-
-class ScoreSystem : Fragment() {
+class ScoreSystem {
 
     val score = 0
 
